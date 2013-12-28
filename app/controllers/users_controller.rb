2ccbf34d48e93314eaaaa415c00701837c535bb5
Jerome Novosad <hello@popzenith.com>
2013-12-28 12:24:54 -0500

    1.037

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -221,7 +221,7 @@ class UsersController < ApplicationController
 
     puts '====================== venues'
 
-    @venues = User.find(params[:id]).venues.paginate(page: params[:page], :per_page => 10)
+    @venues = User.find(params[:id]).venues
     respond_to do |format|
       format.html # index.html.erb
       format.json  { render :json => {
