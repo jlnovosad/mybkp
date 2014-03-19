de2e42bc6394cffb290d3b4c1f9114032ea06d63
Jerome Novosad <hello@popzenith.com>
2014-03-18 21:07:10 -0400

    1.126

--- app/models/drink_category.rb
+++ app/models/drink_category.rb
@@ -4,7 +4,7 @@ class DrinkCategory < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   #########################################
   attr_accessible :drink_id, :category_id
-  validates_uniqueness_of :drink_id, :scope => :category_id
+  #validates_uniqueness_of :drink_id, :scope => :category_id
 
   #########################################
   # refers to the join
