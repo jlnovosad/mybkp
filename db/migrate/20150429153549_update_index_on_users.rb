5d006159f4c6f96ae9e623617e4b6bf4dc884fb9
Jerome Novosad <hello@popzenith.com>
2015-05-16 18:24:05 -0400

    1.167

--- db/migrate/20150429153549_update_index_on_users.rb
+++ db/migrate/20150429153549_update_index_on_users.rb
@@ -1,7 +1,5 @@
 class UpdateIndexOnUsers < ActiveRecord::Migration
   def up
-	  sql = 'DROP INDEX index_users_on_email'
-	  sql << ' ON users' if Rails.env == 'production' # Heroku pg
-	  ActiveRecord::Base.connection.execute(sql)
+	  remove_index(:users, :name => 'index_users_on_email')
 	end
 end
